7db5f824f878c33d08dedac1824eca87ee4836cf
besogogoladze <Gogoladzebeso8@gmail.com>
2026-07-15 14:25:36 +0200

    remove return

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  allowCypressEnv: false,
   e2e: {
     setupNodeEvents(on, config) {
       // import.meta is available in ESM configs
-      return config;
     },
   },
 });
